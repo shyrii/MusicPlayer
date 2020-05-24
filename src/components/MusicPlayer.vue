6fd65e9c94f9e343dbe4ac0a2caeddbb7bad6aa0
shyrii <shyrii0228@gmail.com>
2020-05-24 16:00:49 +0800

    feat: add slider component and player UI

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -14,22 +14,64 @@
         <NextLogo class="small-icon player-icon"/>
       </div>
     </div>
-    
+    <div class="progress-block">
+      <div class="time">01:17</div>
+      <div class="progress-bar">
+        <vue-slider 
+          v-model="sliderValue"
+          :tooltip="'none'"
+          :railStyle="{ backgroundColor: '#c2c2c4' }"
+          :processStyle="{ backgroundColor: '#e93c3c' }"
+          :min='0'
+          :max='totalTime'
+        ></vue-slider>
+      </div>
+      <div class="time">03:12</div>
+    </div>
+    <div class="volume-block">
+      <VolumeLogo class="small-icon"/>
+      <div class="volume-bar">
+        <vue-slider 
+          v-model="volumeValue"
+          :tooltip="'none'"
+          :railStyle="{ backgroundColor: '#c2c2c4' }"
+          :processStyle="{ backgroundColor: '#e93c3c' }"
+          :min='0'
+          :max='totalTime'
+        ></vue-slider>
+      </div>
+      <LyricsLogo class="small-icon"/>
+    </div>
   </div>
 </template>
 <script>
+import VueSlider from 'vue-slider-component'
+import 'vue-slider-component/theme/default.css'
+
 import PrevLogo from '../assets/prev.svg';
 import NextLogo from '../assets/next.svg';
 import PlayLogo from '../assets/play.svg';
 import PauseLogo from '../assets/pause.svg';
+import VolumeLogo from '../assets/volume.svg';
+import LyricsLogo from '../assets/lyrics.svg';
 
 export default {
   name: 'MusicPlayer',
   components: {
+    VueSlider,
     PrevLogo,
     NextLogo,
     PlayLogo,
-    PauseLogo
+    PauseLogo,
+    VolumeLogo,
+    LyricsLogo
+  },
+  data() {
+    return {
+      sliderValue: 0,
+      volumeValue: 0,
+      totalTime: '60'
+    }
   }
 }
 </script>
@@ -49,6 +91,9 @@ export default {
   }
 }
 .player-wrapper {
+  display: flex;
+  align-items: center;
+  box-sizing: content-box;
   left: 0;
   width: 100%;
   height: 60px;
@@ -85,6 +130,28 @@ export default {
       transition-duration: .2s;
     }
   }
-  
+  .progress-block {
+    display: flex;
+    align-items: center;
+    flex-grow: 1;
+    margin: 0 20px;
+    .time {
+      font-size: 13px;
+      color: #505050;
+    }
+    .progress-bar {
+      flex-grow: 1;
+      margin: 0 15px;
+    }
+  }
+  .volume-block {
+    width: 220px;
+    display: flex;
+    align-items: center;
+    .volume-bar {
+      margin: 0 20px 0 5px;
+      width: 100px;
+    }
+  }
 }
 </style>
